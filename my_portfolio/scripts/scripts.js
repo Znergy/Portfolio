99fd6da73a73d19e9857d79518c5c3569743fa8d
Ryan <ryanjonesirl@gmail.com>
2017-01-30 14:02:25 -0800

    Added Info Header
    Added a header with relevant information to the index.html, styles.css,
    and scripts.js.

---
+++ my_portfolio/scripts/scripts.js
@@ -0,0 +1,36 @@
+/*
+Application: Portfolio
+Author: Ryan Jones
+Version: 01/30/2017
+File Name: scripts.js
+*/
+
+/***** Header *****/
+
+/* Nav Buttons */
+
+
+
+/***** Portfolio *****/
+
+/* Project Images */
+
+
+
+/***** Contact *****/
+
+/* Send Button */
+
+
+
+/***** Footer *****/
+
+/* Clickable Icons */
+
+
+
+
+
+
+
+
